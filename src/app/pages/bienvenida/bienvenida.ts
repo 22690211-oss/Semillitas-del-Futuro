dc00a7d4c9483e0187e70c2d3fe796433fdbf4d6
Raymundo <22690211@tecvalles.mx>
2025-11-29 23:19:02 -0600

    Bienvenida

--- src/app/pages/bienvenida/bienvenida.ts
+++ src/app/pages/bienvenida/bienvenida.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+interface Tema {
+  titulo: string;
+  videoUrl: string;
+  thumbnail: string;
+}
+
 @Component({
   selector: 'app-bienvenida',
   standalone: true,
@@ -10,5 +16,33 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./bienvenida.css']
 })
 export class Bienvenida {
-  public mensaje = '¡Bienvenido a Semillitas Futuro!';
+  public mensaje = '¡Bienvenido a Semillitas del Futuro!';
+  
+  public temas: Tema[] = [
+    {
+  titulo: 'Cuidemos el Agua',
+  videoUrl: 'https://www.youtube.com/watch?v=yOGqpFOO2Dk',
+  thumbnail: 'https://img.youtube.com/vi/yOGqpFOO2Dk/maxresdefault.jpg'
+},
+{
+  titulo: 'Cómo cuidar el medio ambiente',
+  videoUrl: 'https://www.youtube.com/watch?v=Gpc1s9qSeVM',
+  thumbnail: 'https://img.youtube.com/vi/Gpc1s9qSeVM/maxresdefault.jpg'
+},
+{
+  titulo: 'Cuidado y conservación del medio ambiente',
+  videoUrl: 'https://www.youtube.com/watch?v=SOzzEWStP00',
+  thumbnail: 'https://img.youtube.com/vi/SOzzEWStP00/maxresdefault.jpg'
+},
+{
+  titulo: 'La deforestación — maneras de cuidar al planeta',
+  videoUrl: 'https://www.youtube.com/watch?v=IcBs3P6BKOY',
+  thumbnail: 'https://img.youtube.com/vi/IcBs3P6BKOY/maxresdefault.jpg'
+}
+
+  ];
+  
+  abrirVideo(url: string): void {
+    window.open(url, '_blank');
+  }
 }
